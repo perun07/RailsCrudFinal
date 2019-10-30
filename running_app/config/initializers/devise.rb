c902d6ffe961a6648cbbe5cfdc66b784499f386a
P Lowe <perun@reagan.com>
2019-10-30 08:22:33 -0400

    updating with errors

--- running_app/config/initializers/devise.rb
+++ running_app/config/initializers/devise.rb
@@ -19,6 +19,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], scope: 'userinfo.email,userinfo.profile'
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
