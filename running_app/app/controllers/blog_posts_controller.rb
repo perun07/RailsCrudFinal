2db218be9c5e1c850262ca96abff1a0b80b4979d
P Lowe <perun@reagan.com>
2019-10-29 20:52:43 -0400

    users unite with posts

--- running_app/app/controllers/blog_posts_controller.rb
+++ running_app/app/controllers/blog_posts_controller.rb
@@ -26,7 +26,7 @@ class BlogPostsController < ApplicationController
   # POST /blog_posts
   # POST /blog_posts.json
   def create
-    @blog_post = BlogPost.new(blog_post_params)
+    @blog_post = BlogPost.new(blog_post_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @blog_post.save
